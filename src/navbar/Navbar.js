b37303105cd333ad60d41b8e742a4907ce012090
Mario Aguayo <mario.aguayo@kipu.health>
2022-08-02 10:50:44 -0700

    Added adblocker fallback for promo zone adertisements. Also added randomized images for advertising

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,8 +1,8 @@
 function Navbar() {
     return (
       <div>
-        <header class="tw-bg-k-purple tw-py-5 tw-shadow-sm lg:tw-static lg:tw-overflow-y-visible">
-          <div class="tw-max-w-7xl tw-px-4 sm:tw-px-6 lg:tw-px-8"></div>
+        <header className="tw-bg-k-purple tw-py-5 tw-shadow-sm lg:tw-static lg:tw-overflow-y-visible">
+          <div className="tw-max-w-7xl tw-px-4 sm:tw-px-6 lg:tw-px-8"></div>
         </header>
       </div>
     );
--- src/signin/Signin.js
+++ src/signin/Signin.js
@@ -3,41 +3,41 @@ import LoginLogo from '../assets/images/login-logo.jpeg'
 
 function Signin() {
   return (
-    <div class="tw-px-10 tw-w-1/2">
-      <img src={LoginLogo} class="tw-w-1/2 tw-mx-auto"/>
-      <div class="tw-py-6">
+    <div className="tw-px-10 tw-w-1/2">
+      <img src={LoginLogo} className="tw-w-1/2 tw-mx-auto"/>
+      <div className="tw-py-6">
         <h1>Staff Sign In Here and Accept Terms of Service</h1>
       </div>
       <UserSignin />
-      <div class="tw-w-2/3 tw-mx-auto tw-py-6">
-        <p class="tw-text-justify">
+      <div className="tw-w-2/3 tw-mx-auto tw-py-6">
+        <p className="tw-text-justify">
           By clicking "Sign In Now and Accept our Terms of Service," you indicate that you have read and agree to be bound to the latest Kipu Systems Terms of Service.
         </p>
       </div>
-      <div class="tw-w-2/3 tw-mx-auto">
-        <div class="tw-py-2">
-          <ul class="tw-space-x-6">
-            <li class="tw-inline-block">
-              <a class="tw-text-sm tw-text-k-true-blue" href="https://kipuhealth.com/terms-of-service/">Terms of Service</a>
+      <div className="tw-w-2/3 tw-mx-auto">
+        <div className="tw-py-2">
+          <ul className="tw-space-x-6">
+            <li className="tw-inline-block">
+              <a className="tw-text-sm tw-text-k-true-blue" href="https://kipuhealth.com/terms-of-service/">Terms of Service</a>
             </li>
-            <li class="tw-inline-block">
-              <a class="tw-text-sm tw-text-k-true-blue" href="https://www.kipu.health/contact-kipu-health/">Customer Support</a>
+            <li className="tw-inline-block">
+              <a className="tw-text-sm tw-text-k-true-blue" href="https://www.kipu.health/contact-kipu-health/">Customer Support</a>
             </li>
-            <li class="tw-inline-block">
-              <a class="tw-text-sm tw-text-k-true-blue" href="https://www.kipu.health/kipu-emr-software/">69% Growth Study</a>
+            <li className="tw-inline-block">
+              <a className="tw-text-sm tw-text-k-true-blue" href="https://www.kipu.health/kipu-emr-software/">69% Growth Study</a>
             </li>
           </ul>
         </div>
         <div>
-          <ul class="tw-space-x-6">
-            <li class="tw-inline-block">
-              <a class="tw-text-sm tw-text-k-true-blue" href="https://testing-mat.kipuworks.com/users/password/new">Forgot Password?</a>
+          <ul className="tw-space-x-6">
+            <li className="tw-inline-block">
+              <a className="tw-text-sm tw-text-k-true-blue" href="https://testing-mat.kipuworks.com/users/password/new">Forgot Password?</a>
             </li>
-            <li class="tw-inline-block">
-              <a class="tw-text-sm tw-text-k-true-blue" href="https://kipuhealth.com/emr-training-calendar/">Training Calendar</a>
+            <li className="tw-inline-block">
+              <a className="tw-text-sm tw-text-k-true-blue" href="https://kipuhealth.com/emr-training-calendar/">Training Calendar</a>
             </li>
-            <li class="tw-inline-block">
-              <a class="tw-text-sm tw-text-k-true-blue" href="https://kipuhealth.com/kipu-billing-rcm-software/">Gold Certified Billers</a>
+            <li className="tw-inline-block">
+              <a className="tw-text-sm tw-text-k-true-blue" href="https://kipuhealth.com/kipu-billing-rcm-software/">Gold Certified Billers</a>
             </li>
           </ul>
         </div>
