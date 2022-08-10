ecf6b38877c6e20a009265a07b54d19e28d2f9cc
Mario Aguayo <mario.aguayo@kipu.health>
2022-08-10 13:07:15 -0700

    Added cookies from Amplify to signin and deletion on signout

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App(){
   Hub.listen('auth', (data) => {
     const event = data.payload.event;
     if (event === "signOut") {
+      deleteCookies()
       setUser({})
     }
     if (event === "signIn") {
@@ -20,8 +21,28 @@ function App(){
     }
   });
 
+  function setCookie(id, value){
+    document.cookie = `${id}=${value};`;
+  }
+
+  function deleteCookies(){
+    document.cookie = "KIPU_SSO_ID_TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+    document.cookie = "KIPU_SSO_ACCES_TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+    document.cookie = "KIPU_SSO_REFRESH_TOKEN=; expires=Thu, 01 Jan 1970 00:00:00 UTC;"
+  }
+
+  function assignCookies(data){
+    var idToken = data.idToken.jwtToken;
+    var accessToken = data.accessToken.jwtToken;
+    var refreshToken = data.refreshToken.token;
+    setCookie('KIPU_SSO_ID_TOKEN', idToken);
+    setCookie('KIPU_SSO_ACCES_TOKEN', accessToken);
+    setCookie('KIPU_SSO_REFRESH_TOKEN', refreshToken);
+  }
+
   async function setCognitoUser(){
     await Auth.currentAuthenticatedUser().then(result => {
+      assignCookies(result.signInUserSession)
       return setUser(result)  
     })
   }
